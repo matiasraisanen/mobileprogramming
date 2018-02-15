ea6e827e8a617437e3a4d6daf448e6e49a1af174
matiasraisanen <33057754+matiasraisanen@users.noreply.github.com>
2018-02-15 19:44:31 +0200

    high score
    updates

--- react_native/guessgame/App.js
+++ react_native/guessgame/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Alert, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert, Button, Image, AsyncStorage } from 'react-native';
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {userinput: '', answer: '', feedback: '', counter: 0}
+    this.state = {userinput: '', answer: '', feedback: '', counter: 0, highscore: 'Not set'}
   }
 
   randomize = () => {
     this.setState({answer: (Math.floor(Math.random() * 100) + 1)});
+  }
+
+
+  getScore = () => {
+    AsyncStorage.getItem('highscore').then((value) => this.setState({ 'highscore': value}));
+  }
+
+  compareScore = () => {
+    if (this.state.highscore == 'Not set') {
+      AsyncStorage.setItem('highscore', (this.state.counter +1).toString());
+      this.getScore();
+    }
+
+    else if ((this.state.counter +1) < parseInt(this.state.highscore)) {
+      AsyncStorage.setItem('highscore', (this.state.counter +1).toString());
+      this.getScore();
+    }
+
   }
 
   guess = () => {
@@ -17,6 +35,7 @@ export default class App extends React.Component {
 
     if (this.state.userinput == this.state.answer) {
       Alert.alert('Number ' + this.state.answer + ' is correct! It took you '+ (this.state.counter +1) +' guesses.');
+      this.compareScore();
       this.setState({userinput: '', counter: 0, feedback: ''});
       this.setState({answer: (Math.floor(Math.random() * 100) + 1)});
     }
@@ -39,12 +58,14 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
+    AsyncStorage.clear()
     this.setState({answer: (Math.floor(Math.random() * 100) + 1)});
   }
 
   render() {
 
     return (
+     
 
       <View style={styles.container}>
 
@@ -59,12 +80,12 @@ export default class App extends React.Component {
             <Text>Answer: {this.state.answer}</Text>
             <Text>Counter: {this.state.counter}</Text>
             */}
-
             <Text>{this.state.feedback}</Text>
+            <Text>High score: {this.state.highscore}</Text>
             <TextInput keyboardType='numeric' style={styles.textinput} 
                 onChangeText={(userinput) => this.setState({userinput})} value={this.state.userinput} />
-            
             <Button onPress={this.guess} title="Guess" />
+
             {/* RANDOMIZER BUTTON
             <Button onPress={this.randomize} title="RNG" />
             */}
